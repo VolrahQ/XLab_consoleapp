203c465651302af4e37a32fbd1d2b9dfa2b19ead
Mikhail Popov <m.popov@xlab-game.com>
2024-10-18 18:09:11 +0300

    0.1.1

---
+++ XLab_console_lesson_project/MyList.cs
@@ -0,0 +1,7 @@
+public class MyList
+{
+    //создать обёртку над массивом из интов (создать свой лист для интов)
+    //функции Add, Remove, RemoveAt, Insert, Clear, ToString
+    private int _count //размер
+    public string ToString() {return }
+}
---
+++ XLab_console_lesson_project/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine($"short - size: {sizeof(short)}");
+
+        Console.WriteLine($"{Convert.ToString(int.MaxValue, 2)}");
+        char c = "c";
+        string s = "string";
+        string s = "abcd";
+        byte[] bm = new byte [8];
+        Console.WriteLine($"{bm}");
+    }
+}
